81053cc5b61894ea7623592d0054fddb03808399
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-09-05 20:44:14 +0500

    Update QUESTION 4.c

--- LAB3 WORK/QUESTION 4.c
+++ LAB3 WORK/QUESTION 4.c
@@ -7,14 +7,16 @@ int main()
    float total_fuel, fuel_cost, c_avg, L1;
    printf("Enter car's fuel average in kilometres per litre");
    scanf("%f", &c_avg);
-   if c_avg<>0
+   if c_avg<=0
+      {
    printf("Invalid car fuel average");
    else 
-   L1=1207/c-avg;
+   L1=1207/c_avg;
    fuel_cost= (L1*fwd_t)+(L1*bwd_t);
    total_fuel=L1+L1;
    printf(" The total fuel was:  \n", total_fuel);
    printf(" The total cost for fuel was:  \n", fuel_cost);
+      }
 }
 
 
