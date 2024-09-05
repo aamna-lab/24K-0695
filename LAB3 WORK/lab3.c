e934aaa6eefc5315773812a2f471024f84861b03
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-09-05 14:52:18 +0500

    Add files via upload

---
+++ LAB3 WORK/lab3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+    int number1, number2, number3;
+    printf("enter two numbers");
+    scanf("%d", &number1 );
+    scanf("%d", &number2 );
+    number3=number2
+    number2=number1
+    number1=number3
+    printf("The nummbers swapped are: "%d", "%d", number1, number2);
+}
+    
+    
+
+
+    
