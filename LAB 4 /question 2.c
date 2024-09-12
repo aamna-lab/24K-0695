8a7be8ca76a1973ea59628a1e3f34f40536c4191
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-09-12 15:01:13 +0500

    Update question 2.c

--- LAB 4 /question 2.c
+++ LAB 4 /question 2.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("Enter your number\n");
+	scanf("%d", &a);
+	if (a>0 &&  a%10==0 && a%2==0){
+	  printf("Valid input");
+	}else
+	  printf("Invalid input");
 
+
+	
+}
