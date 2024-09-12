9f54551af5b5f7d0f7a03881b47fc711b1ab9df9
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-09-12 14:40:29 +0500

    Update question 1.c

--- LAB 4 /question 1.c
+++ LAB 4 /question 1.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+int main(){
+	int a;
+	printf("Enter your number\n");
+	scanf("%d", &a);
+	if (a%3==0){
+	 printf("This number is a multiple of 3\n");
+	}else
+	 printf("This number is not a multiple of 3\n");
+}
