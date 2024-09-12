b46ad79893bffe8542a5a4b50e396dcb7496277d
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-09-12 15:26:39 +0500

    Update question 3.c

--- LAB 4 /question 3.c
+++ LAB 4 /question 3.c
@@ -1,1 +1,28 @@
-
+#include<stdio.h>
+int main(){
+    int num1,num2;
+    char op;
+    printf("Enter an operator (+, -, /, *):\n");
+    scanf("%c" ,&op);
+    printf("Enter your first number\n");
+    scanf("%d",&num1);
+    printf("Enter your second number\n");
+    scanf("%d", &num2);
+    switch(op){
+        case '+':
+          printf("%d + %d = %d", num1, num2, num1+num2);
+        break;
+        case '-':
+          printf("%d - %d = %d", num1, num2, num1-num2);
+        break;
+        case '*':
+          printf("%d*%d=%d", num1, num2,num1*num2);
+        break;
+        case '/' :
+          printf("%d/%d=%d", num1, num2, num1/num2);
+        break;
+        default:
+          printf("Invalid operator");
+    }
+          
+    }
