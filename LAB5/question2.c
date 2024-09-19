5f8d1924f6e94fe1d35aef37764d05cf6c5451ef
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-09-19 14:27:28 +0500

    Update question2.c

--- LAB5/question2.c
+++ LAB5/question2.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main(){
+    int w,v,s;
+    printf("Enter '1' if it is a weekday else enter '2'\n");
+    scanf("%d", &w);
+    printf("Enter '1' if we are on vacation else enter '2'\n");
+    scanf("%d", &v);
+    if (w==2 || v==1){
+        printf("Sleep In!!");
+        
+    }else printf("Cannot Sleep In:(");
+}
