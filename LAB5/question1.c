0b0e226aeef649e4da39f18c66c7ae520b623d6d
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-09-19 14:19:11 +0500

    Update question1.c

--- LAB5/question1.c
+++ LAB5/question1.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main()
+{
+    int c,cf,f;
+    float percent;
+    printf("Enter  total number of calories in food\n");
+    scanf("%d",&c);
+    printf("Enter fat in grams in food\n");
+    scanf("%d",&f);
+    if (f<0 || c<0){
+        printf("Error; either the calories or fat grams were entered incorrectly");
+    }else
+    cf=f*9;
+    if (cf>c){
+        printf("Error; calories from fat greater than total calories");
+    }else if (cf<c){
+    percent=(cf/c)*100;
+    printf("The percentage of calories that come from fat is : %d", cf);
+    }
+    
+    
+}
