8d7a66baab43dd660fcac6d89d34c336599bcad1
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-09-25 14:13:37 +0500

    Update question7.c

--- LAB5/question7.c
+++ LAB5/question7.c
@@ -3,5 +3,5 @@ int main(){
     int x;
     printf("Enter your number\n");
     scanf("%d", &x);
-    x%10==0? printf("Last Digit Zero") : printf("Last Digit Non-zero");
+    x%10==0? printf("Last Digit Zero") : printf("Last Digit Non-zero");;
 }
