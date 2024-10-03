60b15e14825755d99948569cf6dd18f5834257d2
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-10-03 13:40:53 +0500

    Update and rename QUESTION5.C to question5.c

---
+++ LAB 6/question5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a;
+    for (a=2;a<=7;a++){
+        if(a==2 || a==7){
+            printf(" * * * * *\n");
+            
+        }else
+        printf(" *       *\n");
+      
+        }
+    }   
