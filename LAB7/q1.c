cd4bd293be2a750d87b75d8295677162ef2053b0
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-10-10 15:17:19 +0500

    Update q1.c

--- LAB7/q1.c
+++ LAB7/q1.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+int main() {
+   int arr[6]={1,2,3,4,5,6};
+   int i,temp;
+   for(i=0;i<6;i++){
+       temp=arr[i]; //temp2
+       arr[i]=arr[5]; //5
+       arr[5]=temp;  //1
+       printf("%d", arr[i]);
+   }
+}
