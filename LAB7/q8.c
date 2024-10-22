f7f66f46c3432786c92821c389a1ed320856c736
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-10-22 23:37:32 +0500

    Update q8.c

--- LAB7/q8.c
+++ LAB7/q8.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main() {
+char array[50];
+int i;
+printf("enter operation\n");
+scanf("%s", &array);
+for(i=0;i<50;i++){
+    if(array[i]=='+'){
+        printf("add operation : %d + %d = %d\n", array[i-1], array[i+1], array[i-1]+ array[i+1]);
+    }else if(array[i]=='-'){
+         printf("subtract operation : %d + %d = %d\n", array[i-1], array[i+1], array[i-1] - array[i+1]);
+    }
+}
+}
