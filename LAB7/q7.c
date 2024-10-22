b718976aa623827bbd7bbf6330a2071aaef36ed3
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-10-22 22:48:43 +0500

    Update q7.c

--- LAB7/q7.c
+++ LAB7/q7.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main() {
+    int i,n,number,count=0;
+    printf("Enter array size\n");
+    scanf("%d", &n);
+    int arr[n];
+    for(i=1;i<=n;i++){
+        printf("Enter %d element\n",i);
+        scanf("%d", &number);
+        count++;
+    }
+    if(count){
+        printf("Number %d occured more than once", count);
+    }
+}
