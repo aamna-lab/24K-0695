b4852d67793dae219bbf77c6c95c3f069f632a32
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-10-22 22:30:17 +0500

    Update q6.c

--- LAB7/q6.c
+++ LAB7/q6.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+int main() {
+    int arr[10];
+    int n; int i; int number;
+    int count=0;
+    printf(" Enter 10 integers :\n");
+    for(i=0;i<10;i++){
+        scanf("%d", &arr[i]); }
+    for (i=0;i<10;i++){
+        for(n=i+1;n<10;n++){
+            if(arr[i]==arr[n]){
+                count++;
+                number=arr[i];
+            }
+        }
+        if(count>0){
+        printf("The integer %d is repeated %d times\n", number,count);
+    }
+    
+    }
+}
