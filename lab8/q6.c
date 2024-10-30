350d9a10081fc7c1d5264d6e45cfcbc91aa86992
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-10-30 22:03:34 +0500

    Create q6.c

---
+++ lab8/q6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int rows = 5; 
+    for (int i = 0; i < rows; i++) {
+        int value = 1; 
+        for (int j = 0; j < rows - i - 1; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++) {
+            printf("%d ", value);
+            value = value * (i - j) / (j + 1);
+        }
+        printf("\n");
+    }
+}
