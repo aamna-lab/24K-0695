16046bb07d0e55990dde52f795f6552b9b3b108a
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-10-31 13:54:25 +0500

    Update q2.c

--- lab 9/q2.c
+++ lab 9/q2.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+    void swapInteger(int n1,int n2){
+        int temp;
+        temp=n1;
+        n1=n2; 
+        n2=temp;
+        printf("first number is %d , second number is %d", n1,n2);
+        
+    }
+int main(){
+    int a,b;
+    printf("Enter 1st number\n");
+    scanf("%d", &a);
+    printf("Enter 2nd number\n");
+    scanf("%d",&b);
+    swapInteger(a,b);
+}
