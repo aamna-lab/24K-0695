27c89111b1e64b4b329b885f82c761d5ee869adb
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-11-18 20:56:59 +0500

    Create q3.c

---
+++ lab 10/q3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int compare(float t1){}
+int main(){
+    int ans,choice;
+    float temp;
+    const int maxT= 1000;
+    int compare(float t1){
+    int count;
+    if(t1>maxT){
+        count++;
+    }
+   
+}
+do{
+printf("Enter '1' to enter temp or '0' to exit\n");
+scanf("%d", &choice);
+printf("Enter temperature\n");
+        scanf("%f", &temp);
+}while(choice==1);
+        ans= compare(temp);
+}
