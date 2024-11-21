f2a450aacb1324f5ae99da7182a3a4175a488c0c
Aamna Rizwan <aamnarizwan7@gmail.com>
2024-11-21 14:46:53 +0500

    Update q1.c

--- lab11/q1.c
+++ lab11/q1.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main(){
+	char buffer[30];
+	FILE *ptr = fopen("idk.txt", "w");
+	fprintf(ptr, "Example d4ta");
+	fclose(ptr);
+	FILE *ptrRead = fopen("idk.text", "r");
+	FILE *ptr2 = fopen("idk1.txt" , "w");
+	while ( fgets(buffer, sizeof(buffer), ptrRead)  != NULL){
+		fputs(buffer, ptr2);
+	}
+	fclose(ptrRead);
+	fclose(ptr2);
+}
