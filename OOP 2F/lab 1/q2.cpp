9be7148089a8edf7ac3090c5867cd973bb8f4a47
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-01-20 09:12:15 +0500

    Update q2.cpp

--- OOP 2F/lab 1/q2.cpp
+++ OOP 2F/lab 1/q2.cpp
@@ -11,20 +11,21 @@ int main() {
         for (int j=1;j<=3;j++){
             cout << "Enter marks for subject " << j << " for student " << i << endl;
             cin >> marks;
-            if (marks>=90){
-                cout << "Grade for Subject " << j << " = A" << endl;
-            }else if (marks>=80 && marks<=89){
-                cout << "Grade for Subject " << j << " = B" << endl;
-            }else if (marks>=70 && marks<=79){
-                cout << "Grade for Subject " << j << " = C" << endl;
-            }else if(marks>=60 && marks<=69){
-                cout << "Grade for Subject " << j << " = D" << endl;
-            }else 
-            cout << "Grade for Subject" << j << "= F" << endl;
           total=total+marks;  
         }
         cout << "Total marks for student " << i << " = " << total << endl;
         cout << "Average marks for student " << i << " = " << total/3 << endl;
+        avg= total/3;
+        if (avg>=90){
+                cout << "Grade = A" << endl;
+            }else if (avg>=80 && avg<=89){
+                cout << "Grade  = B" << endl;
+            }else if (avg>=70 && avg<=79){
+                cout << "Grade = C" << endl;
+            }else if(avg>=60 && avg<=69){
+                cout << "Grade  = D" << endl;
+            }else 
+            cout << "Grade =  F" << endl;
         
     }
 
