ad428edc207d6c30664ac690a90b0b2d1b91d63a
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-02-10 08:40:21 +0500

    Update q1.cpp

--- OOP 2F/lab 4/q1.cpp
+++ OOP 2F/lab 4/q1.cpp
@@ -1,1 +1,55 @@
+#include <iostream>
+using namespace std;
+class Student {
+private:
+    int studentId;
+    string name;
+    int age;
+    char grade; 
+public:
+    Student() {
+        studentId = 0;
+        age = 0;
+        grade = 'N'; 
+        name = "";
+    }
+    Student(int id, int age1, char grade1, string n) {
+        studentId = id;
+        age = age1;
+        grade = grade1;
+        name = n;
+    }
+    int getAge() {
+        return age;
+    }
+    string getName() {
+        return name;
+    }
+    char getGrade() {
+        return grade;
+    }
+    int getId() {
+        return studentId;
+    }
+    void isEligibleForScholarship() {
+        if (grade == 'A') {
+            cout << "Eligible for Scholarship" << endl;
+        } else {
+            cout << "Not Eligible for Scholarship" << endl;
+        }
+    }
+    void displayDetails() {
+        cout << "Name: " << getName() << endl;
+        cout << "Student Id: " << getId() << endl;
+        cout << "Age: " << getAge() << endl;
+        cout << "Grade: " << getGrade() << endl;
+    }
+};
+
+int main() {
+    Student object(240695, 18, 'A', "Aamna");
+    object.displayDetails();
+    object.isEligibleForScholarship();
+
+}
 
