e6414740bae2f53551f63b6c5675e59d81199cad
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-03-30 12:19:50 +0500

    Update q4.cpp

--- OOP 2F/oop assignment 2/q4.cpp
+++ OOP 2F/oop assignment 2/q4.cpp
@@ -1,58 +1,98 @@
+//24K-0695 Aamna Rizwan_Q4
 #include <iostream>
+#include <string>
 using namespace std;
 
-class User{
-    protected:
-    string name;
-    int ID;
-    string permission[10];
-    string email;
-    //hashed password
-    public:
-    //constructor
-    virtual void display(){
-        cout << "Name : " << name <<  endl;
-        cout << "ID : " << id <<  endl;
-         cout << "Email : " << email<<  endl;
-    }
-    //access lab function
-};
-
-class Student : public User{
-    protected:
-    bool assignment[5];
-    public:
-    //constructor
-    void display() override{
-        User :: display();
-    }
-    //list of assignment
-    //functuonality
-};
-
-class TA : public Student{
-    protected:
-    string projects[2];
-    public:
-    //constructor
-    //function to display projects
-    //override display func
-    //TA can manage onlt 10 studnets
-};
-
-class Professor : public User{
-    public:
-    //override display
-    //functionality to allow ta's to work with them
-};
-
-void authenticateAndPerformAction (User* user,string action){
-    
+int passwordGeneration(char c) {
+    int hash = 5381;
+    return (hash * 33) + c;
 }
 
+string hashPassword(const string &password) {
+    int hash = 5381;
+    for (char c : password) {
+        hash = (hash * 33) + c;
+    }
+    return to_string(hash);
+}
 
+class User {
+protected:
+    string name, email;
+    int id;
+    string hashed_password;
+    string permissions[10];
+    int permCount;
+public:
+    User(string n, int i, string e, string p) : name(n), id(i), email(e), hashed_password(hashPassword(p)), permCount(0) {}
+    virtual bool authenticate(string password) {
+         return hashPassword(password) == hashed_password; 
+    }
+    virtual void display() { 
+        cout << "Name: " << name << ", ID: " << id << ", Email: " << email << endl; 
+    }
+    virtual bool accessLab() {
+         return false; 
+        }
+};
 
+class Student : public User {
+protected:
+    bool assignments[5];
+public:
+    Student(string n, int i, string e, string p) : User(n, i, e, p) {
+        for (int j = 0; j < 5; j++) assignments[j] = false;
+    }
+    void submitAssignment(int index) { if (index >= 0 && index < 5) assignments[index] = true; }
+    void display() override {
+        User::display();
+        cout << "Assignments: ";
+        for (int j = 0; j < 5; j++) cout << assignments[j] << " ";
+        cout << endl;
+    }
+};
+
+class TA : public Student {
+protected:
+    string projects[2];
+    Student* assignedStudents[10];
+    int projectCount, studentCount;
+public:
+    TA(string n, int i, string e, string p) : Student(n, i, e, p), projectCount(0), studentCount(0) {}
+    void assignStudent(Student *s) { if (studentCount < 10) assignedStudents[studentCount++] = s; }
+    void addProject(string project) { if (projectCount < 2) projects[projectCount++] = project; }
+    void display() override {
+        Student::display();
+        cout << "Projects: ";
+        for (int j = 0; j < projectCount; j++) cout << projects[j] << " ";
+        cout << endl;
+    }
+};
+
+class Professor : public User {
+public:
+    Professor(string n, int i, string e, string p) : User(n, i, e, p) {}
+    void display() override { User::display(); }
+};
+
+void authenticateAndPerformAction(User* user, string action) {
+    if (action == "access_lab" && user->accessLab()) {
+        cout << "Lab access granted" << endl;
+    } else {
+        cout << "Action not allowed" << endl;
+    }
+}
 
 int main() {
- 
+    Student s("Aamna", 1, "k240695@nu.edu.pk", "password");
+    TA t("Laiba",2, "k240644@nu.edu.pk", "password");
+    Professor p("Ms.Abeeha Sattar", 3455, "abeeha.sattar@nu.edu.pk", "password");
+
+    s.submitAssignment(0);
+    t.addProject("Project 1");
+    t.assignStudent(&s);
+    s.display();
+    t.display();
+    p.display();
+    authenticateAndPerformAction(&t, "access_lab");
 }
