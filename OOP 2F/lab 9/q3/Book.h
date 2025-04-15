31a72cc17857a64c569dec10f507ea78d533caa5
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-04-15 14:18:11 +0500

    Update Book.h

--- OOP 2F/lab 9/q3/Book.h
+++ OOP 2F/lab 9/q3/Book.h
@@ -1,1 +1,21 @@
 
+#ifndef BOOK
+#define BOOK
+
+#include <string>
+using namespace std;
+class Book{
+    string title;
+    string author;
+    string ISBN;
+
+    public:
+    Book(string t, string a, string isbn);
+
+    string getTtile();
+    string getAuthor();
+    string getISBN();
+
+};
+
+#endif
