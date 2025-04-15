028c3bcec8864773de26c3e3d2644cb9911b6b66
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-04-15 14:21:04 +0500

    Update Library.h

--- OOP 2F/lab 9/q3/Library.h
+++ OOP 2F/lab 9/q3/Library.h
@@ -1,1 +1,19 @@
+#ifndef LIBRARY
+#define LIBRARY
 
+#include <string>
+#include "Book.h"
+
+class Library{
+    Book** books;
+    int bookCount;
+
+    public:
+        Library(int count);
+
+        void addBook();
+        void removeBook();
+        void searchBook();
+};
+
+#endif
