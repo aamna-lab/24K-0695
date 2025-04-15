17156bd98a0301341b51896b3fc0a77e0e3cea5e
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-04-15 14:15:16 +0500

    Update Book.cpp

--- OOP 2F/lab 9/q3/Book.cpp
+++ OOP 2F/lab 9/q3/Book.cpp
@@ -1,1 +1,17 @@
+#include "Book.h"
+#include <iostream>
+using namespace std;
 
+Book::Book(string t, string a, string isbn) : title(t), author(a), ISBN(isbn) {}
+
+string Book::getTtile() {
+    return title;
+}
+
+string Book::getAuthor(){
+    return author;
+}
+
+string Book::getISBN(){
+    return ISBN;
+}
