fe28ad2f77d582e3e58f876990754255224c6945
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-04-15 13:45:32 +0500

    Update q1.cpp

--- OOP 2F/lab 9/q1.cpp
+++ OOP 2F/lab 9/q1.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+using namespace std;
 
+class Vehicle{
+    protected:
+    string model;
+    double rate;
+    public:
+    Vehicle (string m,double r) : model(m) , rate(r) {}
+    virtual double getDailyRate() = 0;
+    virtual void displayDetails() = 0;
+};
+
+class Car : public Vehicle{
+    public :
+    Car (string m,double r) : Vehicle(m,r) {}
+    double getDailyRate() override{
+        return rate;
+    }
+    void displayDetails() override{
+        cout << " Car Model Name : " << model << endl;
+        cout << " Car Daily Rate : " << getDailyRate() << endl;
+    }
+};
+
+class Bike : public Vehicle{
+    public :
+    Bike (string m,double r) : Vehicle(m,r) {}
+    double getDailyRate() override{
+        return rate;
+    }
+    void displayDetails() override{
+        cout << " Bike Model Name : " << model << endl;
+        cout << " Bike Daily Rate : " << getDailyRate() << endl;
+    }
+};
+
+
+int main() {
+   Car c("Toyota 345", 8.45);
+   Bike b("Hyundai 234" , 7.45);
+   c.displayDetails();
+   b.displayDetails();
+   cout << "Daily Rates of car : " << c.getDailyRate() << endl;
+    cout << "Daily Rates of bike : " << b.getDailyRate() << endl;
+  
+}
