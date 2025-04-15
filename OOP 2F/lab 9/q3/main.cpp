a50767d853f578106ef2daba58340f5c3f2a0b72
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-04-15 14:25:35 +0500

    Update main.cpp

--- OOP 2F/lab 9/q3/main.cpp
+++ OOP 2F/lab 9/q3/main.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include "Book.h"
+#include "Library.h"
+using namespace std;
 
+int main() {
+    Book book("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565");
+    cout << "Book Title: " << book.getTitle() << endl;
+    cout << "Book Author: " << book.getAuthor() << endl;
+    cout << "Book ISBN: " << book.getISBN() << endl;
+
+    return 0;
+}
