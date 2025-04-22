3a42e6ce4cb5f2ec12ba5cd891fb161c4c812ffb
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-04-22 13:52:06 +0500

    Update q2.cpp

--- OOP 2F/lab 10 /q2.cpp
+++ OOP 2F/lab 10 /q2.cpp
@@ -1,1 +1,48 @@
+#include <iostream>A
+#include <fstream>
+using namespace std;
 
+int main(){
+    string name;
+    string email;
+    int yearsOfExperience;
+    string summary;
+    cout << "Enter your name\n" ; 
+    getline(cin,name);
+    cout << "Enter email\n"; 
+    getline(cin,email);
+    cout << "Enter your number of years of experience\n"; cin >> yearsOfExperience;
+    cin.ignore();
+    cout << "Enter a summary of your experience\n"; 
+    getline(cin,summary);
+
+    ofstream file;
+    file.open("resume.txt", ios::trunc);
+
+    if(!file){
+        cerr << "Error : File could not be created\n";
+        return 1;
+    }
+
+    file << "Name : " << name << endl;
+    file << "Email : " << email << endl;
+    file << "Years : " << yearsOfExperience << endl;
+    file << "Summary : " << summary << endl;
+
+    file.close();
+
+    ifstream readFile("resume.txt");
+    if(!readFile){
+        cerr << "Error : File could not be opened for reading;\n";
+        return 1;
+    }
+
+    string line;
+    cout << "Info in file: \n";
+    while (getline(readFile, line)){
+        cout << line << endl;
+    }
+
+    readFile.close();
+
+}
