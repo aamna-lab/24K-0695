f4e36358b6f3b190a5aa85bd56f0792824bcf7cd
Aamna Rizwan <aamnarizwan7@gmail.com>
2025-05-02 20:52:07 +0500

    Create q4.cpp

---
+++ OOP 2F/lab 11/q4.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <typeinfo>
+#include <cmath>
+using namespace std;
+
+class NegativeNumberException : public exception {
+public:
+    const char* what() const throw() {
+        return "NegativeNumberException - Input must be non-negative!";
+    }
+};
+
+class InvalidTypeException : public exception {
+public:
+    const char* what() const throw() {
+        return "InvalidTypeException - Non-numeric type detected!";
+    }
+};
+
+template <typename T>
+T mysqrt(T num) {
+    if (typeid(T) != typeid(int) && typeid(T) != typeid(double)) {
+        throw InvalidTypeException();
+    }
+    if (num < 0) {
+        throw NegativeNumberException();
+    }
+    return sqrt(num);
+}
+
+int main() {
+    try {
+        cout << mysqrt(-4) << endl;
+    } catch (exception& e) {
+        cout << "sqrt(-4): " << e.what() << endl;
+    }
+
+    try {
+        string s = "hello";
+        throw InvalidTypeException(); // mimic invalid template instantiation
+    } catch (exception& e) {
+        cout << "sqrt(\"hello\"): " << e.what() << endl;
+    }
+
+    return 0;
+}
